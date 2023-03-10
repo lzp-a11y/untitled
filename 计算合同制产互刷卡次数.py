982f555ee4c3de458e1ce0d5b6482fdb07e99a3c
lzp-a11y <765540196@qq.com>
2023-03-10 16:05:15 +0800

    计算合同制刷卡次数修改

--- 计算合同制产互刷卡次数.py
+++ 计算合同制产互刷卡次数.py
@@ -3,14 +3,14 @@ import os
 import time
 start_time = time.time()
 root_dir = os.path.dirname(__file__)                 # 获取当前项目所在目录地址
-excl1 = os.path.join(root_dir, "1月员工资金流水.xls")   # 拼接excel表格地址
+excl1 = os.path.join(root_dir, "2月份员工资金变动流水.xls")   # 拼接excel表格地址
 excl2 = os.path.join(root_dir, "林倩名单.xlsx")
 excl3 = os.path.join(root_dir, "张怡君名单.xlsx")
 excl4 = os.path.join(root_dir, "合同制员工刷卡次数统计.xlsx")
 
 df1 = pd.read_excel(excl1, sheet_name='员工资金变动流水', skiprows=1)   # 省略指定行数，省略第一行
 df2 = pd.read_excel(excl2, sheet_name='Sheet1')               # 林倩名单
-df3 = pd.read_excel(excl3, sheet_name='Sheet1')               # 张怡君名单
+df3 = pd.read_excel(excl3, sheet_name='Sheet2')               # 张怡君名单
 
 # how 合并方式，left左连接，保留left的全部数据， on： 链接的列属性。
 result = pd.merge(df1, df2.loc[:, ['姓名', '人员类型', '现部门', '二级机构']], how='left', on='姓名')
@@ -33,9 +33,18 @@ result5 = pd.concat([result3, result4], ignore_index=True)   # 拼接result3和4
 result6 = result2.loc[filter5 & filter10]      # 合同制且状态为出账
 result7 = pd.concat([result6, result5, result5]).drop_duplicates(keep=False)
 
+num1 = len(result5.index)     # 产互刷卡次数
+num2 = len(result7.index)     # 非产互刷卡次数
+result5['刷卡总次数'] = None
+result7['刷卡总次数'] = None
+result5.iloc[0, 19] = num1
+result7.iloc[0, 19] = num2
+
+
 new_wb = pd.ExcelWriter(excl4)                    # 使用ExcelWriter()可以向同一个excel的不同sheet中写入对应的表格数据
 result5.to_excel(new_wb, sheet_name='产互刷卡次数', index=False)     # 保存result5的数据到非合同制主业 页
-result7.to_excel(new_wb, sheet_name='非产互刷卡次数', index=False)     # 保存result6的数据到非合同制主业 页
+result7.to_excel(new_wb, sheet_name='非产互刷卡次数', index=False)     # 保存result7的数据到非合同制主业 页
+
 new_wb.close()            # 直接调用关闭接口就可以了，close方法里面有save保存函数
 end_time = time.time()
 run_time = end_time - start_time
